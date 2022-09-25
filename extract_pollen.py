3ab5c0cb6bbf87d09d6ba1ac515427da3766d810
kavidey <kaviasher@gmail.com>
2022-09-25 15:22:15 -0700

    Clean up code and add documentation

--- extract_pollen.py
+++ extract_pollen.py
@@ -2,8 +2,6 @@
 import pandas as pd
 import numpy as np
 import pathlib
-import logging
-import datetime
 import cv2 as cv
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
@@ -11,11 +9,8 @@ import skimage
 import skimage.feature
 import skimage.transform
 import skimage.draw
-import sklearn.cluster
 import skimage.segmentation
 import scipy
-from collections import Counter
-
 # %%
 pollen_slides_dir = "pollen_slides"
 pollen_slides_database_name = "database.csv"
@@ -24,6 +19,7 @@ pollen_slides_df = pd.read_csv(
     pathlib.Path(pollen_slides_dir) / pollen_slides_database_name
 )
 # %%
+# From: https://www.codepasta.com/computer-vision/2019/04/26/background-segmentation-removal-with-opencv-take-2.html
 def filterOutSaltPepperNoise(edgeImg):
     # Get rid of salt & pepper noise.
     count = 0
@@ -40,10 +36,8 @@ def filterOutSaltPepperNoise(edgeImg):
         lastMedian = median
         median = cv.medianBlur(edgeImg, 3)
 
-# %%
-# %matplotlib widget
-import ipywidgets as widgets
 
+# %%
 dim = 5
 fig = plt.figure(figsize=(10.0, 10.0))
 grid = ImageGrid(
@@ -53,6 +47,7 @@ grid = ImageGrid(
     # axes_pad=0.1,  # pad between axes in inch.
 )
 
+# Filter out any 100x images
 pollen_slides_400x_filtered_df = pollen_slides_df[
     pollen_slides_df["image_magnification"] == 400
 ]
@@ -63,46 +58,47 @@ chosen_idx = np.random.choice(
 
 img_downscale = 5
 
-edge_detector = cv.ximgproc.createStructuredEdgeDetection('model.yml')
+edge_detector = cv.ximgproc.createStructuredEdgeDetection("model.yml")
 
 for i, (index, row) in enumerate(
     pollen_slides_400x_filtered_df.iloc[chosen_idx].iterrows()
 ):
-    # slide_img = cv.imread(row["path"])
-    # slide_imgs.append(slide_img)
-    slide_img = slide_imgs[i]
+    slide_img = cv.imread(row["path"])
     slide_img = cv.resize(
         slide_img,
         (
-            int(slide_img.shape[1] /     img_downscale),
+            int(slide_img.shape[1] / img_downscale),
             int(slide_img.shape[0] / img_downscale),
         ),
     )
 
+    # Normalizes the image (https://docs.opencv.org/3.4/d2/de8/group__core__array.html#ga87eef7ee3970f86906d69a92cbf064bd)
+    # I don't fully know how this works, but it effectively increases local contrast
     norm = np.zeros(slide_img.shape)
     slide_img_normalized = cv.normalize(slide_img, norm, 0, 255, cv.NORM_MINMAX)
-    # slide_img_blurred = cv.medianBlur(slide_img, 5)
-    # slide_img_blurred = cv.pyrMeanShiftFiltering(slide_img_blurred, 11, 30)
-    # image_to_detect = slide_img_blurred[:, :, 1]
-    # slide_img = slide_img_blurred
 
-    slide_img_normalized = (slide_img_normalized - np.min(slide_img_normalized, axis=(0, 1))) / (
-        np.max(slide_img_normalized, axis=(0, 1)) - np.min(slide_img_normalized, axis=(0, 1))
+    # This remaps the range of values in an image to fill the full 0-255 range
+    slide_img_normalized = (
+        slide_img_normalized - np.min(slide_img_normalized, axis=(0, 1))
+    ) / (
+        np.max(slide_img_normalized, axis=(0, 1))
+        - np.min(slide_img_normalized, axis=(0, 1))
     )
     slide_img_normalized = (slide_img_normalized * 255).astype(np.uint8)
     image_to_detect = slide_img_normalized
 
-    # slide_img_hsv = cv.cvtColor(image_to_detect, cv.COLOR_BGR2HSV)
-    # slide_img_lab = cv.cvtColor(image_to_detect, cv.COLOR_BGR2LAB)
-    # to_draw_img = cv.cvtColor(image_to_detect, cv.COLOR_GRAY2BGR)
-
+    # Run an random forest edge detector
     edges = edge_detector.detectEdges(image_to_detect.astype(np.float32) / 255.0)
     filterOutSaltPepperNoise(edges)
 
-    contours, hierarchy = cv.findContours(((edges**2)*255).astype(np.uint8), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # Find the contours on the edges
+    contours, hierarchy = cv.findContours(
+        ((edges**2) * 255).astype(np.uint8), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
+    )
     # draw the contours on a copy of the original image
     cv.drawContours(image_to_detect, contours, -1, (255, 0, 0), 2)
 
+    # Turn the contours into a black and white mask
     mask = np.zeros_like(edges)
     cv.fillPoly(mask, contours, 255)
     mapFg = cv.erode(mask, np.ones((5, 5), np.uint8), iterations=10)
@@ -115,14 +111,14 @@ for i, (index, row) in enumerate(
     bgdModel = np.zeros((1, 65), np.float64)
     fgdModel = np.zeros((1, 65), np.float64)
     rect = (1, 1, slide_img.shape[1], slide_img.shape[0])
-    cv.grabCut(image_to_detect, trimap, rect, bgdModel, fgdModel, 5, cv.GC_INIT_WITH_MASK)
+    cv.grabCut(
+        image_to_detect, trimap, rect, bgdModel, fgdModel, 5, cv.GC_INIT_WITH_MASK
+    )
 
     # create mask again
-    mask2 = np.where(
-        (trimap == cv.GC_FGD) | (trimap == cv.GC_PR_FGD),
-        255,
-        0
-    ).astype('uint8')
+    mask2 = np.where((trimap == cv.GC_FGD) | (trimap == cv.GC_PR_FGD), 255, 0).astype(
+        "uint8"
+    )
     slide_img_masked = slide_img * mask2[:, :, np.newaxis]
 
     kernel3 = np.ones((3, 3), np.uint8)
@@ -141,7 +137,9 @@ for i, (index, row) in enumerate(
 
     # Watershed Code
     dist = scipy.ndimage.distance_transform_edt(opening)
-    peak_idx = skimage.feature.peak_local_max(dist, min_distance=10, threshold_rel=0.5, labels=opening)
+    peak_idx = skimage.feature.peak_local_max(
+        dist, min_distance=10, threshold_rel=0.5, labels=opening
+    )
 
     local_max = np.zeros_like(dist, dtype=bool)
     local_max[tuple(peak_idx.T)] = True
@@ -153,7 +151,11 @@ for i, (index, row) in enumerate(
     markers_rounded = markers.astype(np.uint8)
     contours = []
     for j in range(1, markers_rounded.max() + 1):
-        c, _ = cv.findContours(np.array(markers_rounded == j).astype(np.uint8), cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+        c, _ = cv.findContours(
+            np.array(markers_rounded == j).astype(np.uint8),
+            cv.RETR_LIST,
+            cv.CHAIN_APPROX_NONE,
+        )
         contours += c
 
     # Draw all contours on the image (these are drawn in blue, the ones we use will be drawn in green later)
@@ -164,7 +166,7 @@ for i, (index, row) in enumerate(
     for c in contours:
         # Fit a circle to the contour. If the contour doesn't fill 40% of the circle, skip it
         _, r = cv.minEnclosingCircle(c)
-        if cv.contourArea(c) / (np.pi*r**2) < 0.4:
+        if cv.contourArea(c) / (np.pi * r**2) < 0.4:
             continue
 
         # If the of the contour is less than 100 pixels, skip it
