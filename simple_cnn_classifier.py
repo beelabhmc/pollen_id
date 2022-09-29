e65aaca70f5ef34646e4a537ba68d1da9fc23368
Kavi Dey <kaviasher@gmail.com>
2022-09-28 23:16:38 -0700

    Add additional metrics

--- simple_cnn_classifier.py
+++ simple_cnn_classifier.py
@@ -6,7 +6,6 @@ import numpy as np
 
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 import torchmetrics
 
 import pathlib
@@ -16,15 +15,19 @@ from PIL import Image
 import torchvision.transforms as transforms
 
 from tqdm.autonotebook import tqdm
+from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
+import pandas as pd
+import seaborn as sn
+
 # %%
 pollen_grains_dir = pathlib.Path("pollen_grains")
 
-torch.manual_seed(42) 
+torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 BATCH_SIZE = 32
-NUM_WORKERS = 0 # os.cpu_count()
+NUM_WORKERS = 0  # os.cpu_count()
 # %%
 image_res = 64
 train_transform = transforms.Compose(
@@ -95,12 +98,14 @@ test_loader = torch.utils.data.DataLoader(
     shuffle=False,
 )
 
+
 def imshow(img):
-  ''' function to show image '''
-  img = img / 2 + 0.5 # unnormalize
-  npimg = img.numpy() # convert to numpy objects
-  plt.imshow(np.transpose(npimg, (1, 2, 0)))
-  plt.show()
+    """function to show image"""
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()  # convert to numpy objects
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
 
 # get random training images with iter function
 dataiter = iter(train_loader)
@@ -110,47 +115,54 @@ images, labels = dataiter.next()
 imshow(torchvision.utils.make_grid(images))
 
 # print the class of the image
-print(' '.join('%s' % full_pollen_dataset.classes[labels[j]] for j in range(BATCH_SIZE)))
+print(
+    " ".join("%s" % full_pollen_dataset.classes[labels[j]] for j in range(BATCH_SIZE))
+)
 
 # %%
 class TinyVGG(nn.Module):
     """
-    Model architecture copying TinyVGG from: 
+    Model architecture copying TinyVGG from:
     https://poloclub.github.io/cnn-explainer/
     """
+
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
         super().__init__()
         self.conv_block_1 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape, 
-                      out_channels=hidden_units, 
-                      kernel_size=3, # how big is the square that's going over the image?
-                      stride=1, # default
-                      padding=1), # options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
+            nn.Conv2d(
+                in_channels=input_shape,
+                out_channels=hidden_units,
+                kernel_size=3,  # how big is the square that's going over the image?
+                stride=1,  # default
+                padding=1,
+            ),  # options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number
             nn.ReLU(),
-            nn.Conv2d(in_channels=hidden_units, 
-                      out_channels=hidden_units,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1),
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2,
-                         stride=2) # default stride value is same as kernel_size
+            nn.MaxPool2d(
+                kernel_size=2, stride=2
+            ),  # default stride value is same as kernel_size
         )
         self.conv_block_2 = nn.Sequential(
             nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2),
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            # Where did this in_features shape come from? 
+            # Where did this in_features shape come from?
             # It's because each layer of our network compresses and changes the shape of our inputs data.
-            nn.Linear(in_features=hidden_units*16*16,
-                      out_features=output_shape)
+            nn.Linear(in_features=hidden_units * 16 * 16, out_features=output_shape),
         )
-    
+
     def forward(self, x: torch.Tensor):
         x = self.conv_block_1(x)
         # print(x.shape)
@@ -162,54 +174,69 @@ class TinyVGG(nn.Module):
         # return self.classifier(self.conv_block_2(self.conv_block_1(x))) # <- leverage the benefits of operator fusion
 
 
-net = TinyVGG(input_shape=3, # number of color channels (3 for RGB) 
-                  hidden_units=10, 
-                  output_shape=len(full_pollen_dataset.classes)).to(device)
+model = TinyVGG(
+    input_shape=3,  # number of color channels (3 for RGB)
+    hidden_units=10,
+    output_shape=len(full_pollen_dataset.classes),
+).to(device)
 
 criterion = nn.CrossEntropyLoss()
-# optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-optimizer = torch.optim.Adam(params=net.parameters(), lr=0.001)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+# optimizer = torch.optim.Adam(params=net.parameters(), lr=0.001)
 # %%
-for epoch in range(10):  # loop over the dataset multiple times
+metric = torchmetrics.Accuracy().to(device)
+
+for epoch in range(50):  # loop over the dataset multiple times
     with tqdm(train_loader, unit="batch") as tepoch:
         for data, target in tepoch:
             tepoch.set_description(f"Epoch {epoch}")
 
             data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
-            output = net(data)
+            output = model(data)
             predictions = output.argmax(dim=1, keepdim=True).squeeze()
             # loss = F.nll_loss(output, target)
             loss = criterion(output, target)
-            correct = (predictions == target).sum().item()
-            accuracy = correct / BATCH_SIZE
-            
+            accuracy = metric(predictions, target)
+
             loss.backward()
             optimizer.step()
 
-            tepoch.set_postfix(loss=loss.item(), accuracy=100. * accuracy)
+            tepoch.set_postfix(loss=loss.item(), accuracy=100.0 * accuracy.item())
 
-print('Finished Training')
+accuracy = metric.compute()
+print(f"Finished Training. Accuracy: {accuracy}")
 # %%
-# prepare to count predictions for each class
-correct_pred = {classname: 0 for classname in full_pollen_dataset.classes}
-total_pred = {classname: 0 for classname in full_pollen_dataset.classes}
+combined_labels = []
+combined_predictions = []
 
-# again no gradients needed
+model.eval()
 with torch.no_grad():
     for data in test_loader:
         images, labels = data[0].to(device), data[1].to(device)
-        outputs = net(images)
-        _, predictions = torch.max(outputs, 1)
-        # collect the correct predictions for each class
-        for label, prediction in zip(labels, predictions):
-            if label == prediction:
-                correct_pred[full_pollen_dataset.classes[label]] += 1
-            total_pred[full_pollen_dataset.classes[label]] += 1
+        output = model(images)
+        output = (torch.max(torch.exp(output), 1)[1]).data.cpu().numpy()
 
+        labels = labels.data.cpu().numpy()
+        combined_labels.extend(labels)
+        combined_predictions.extend(output)
 
-# print accuracy for each class
-for classname, correct_count in correct_pred.items():
-    accuracy = 100 * float(correct_count) / total_pred[classname]
-    print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+# %%
+metric.reset()
+accuracy = metric(torch.tensor(combined_predictions), torch.tensor(combined_labels))
+print("accuracy", accuracy)
+# %%
+# Confusion Matrix
+cf_matrix = confusion_matrix(combined_labels, combined_predictions)
+df_cm = pd.DataFrame(
+    cf_matrix,
+    index=[i for i in full_pollen_dataset.classes],
+    columns=[i for i in full_pollen_dataset.classes],
+)
+plt.figure(figsize=(12, 7))
+plt.xlabel('predicted')
+plt.xlabel('true label')
+sn.heatmap(df_cm, annot=True)
+# %%
+precision_recall_fscore_support(np.array(combined_labels), np.array(combined_predictions))
 # %%
